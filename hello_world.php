74cd9251818687511dbd0f14c4940cf2fbf25419
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-15 10:14:47 -0500

    no real change

--- hello_world.php
+++ hello_world.php
@@ -4,3 +4,11 @@ $firstName = 'Micah';
 $lastName = 'Johnson';
 
 echo 'Hello World!! My name is ' . $firstName . ' ' . $lastName . '.' . PHP_EOL;
+
+
+// practice arrays!
+$arrayOfNerds = [
+    'first_name' => 'Micah',
+    'last_name' => 'Johnson',
+
+];
