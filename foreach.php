642623dcd294e2f6201abea1c99bac3cfdefa3e1
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-18 15:23:41 -0500

    6.3.2 Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
    Construct a loop that iterates through each value and outputs only values with a type that is scalar.
    
    Create a loop that will echo out every value, including those nested in arrays. Output should look like this.

--- foreach.php
+++ foreach.php
@@ -1,19 +1,30 @@
 <?php
 
-$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+$entireArray = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach($things as $random){
-    if (is_integer($random)) {
-        echo "{$random} is an array\n";
-    } else if (is_float($random)) {
-        echo "{$random} is a float\n";
-    } else if (is_bool($random)) {
-        echo "{$random} is a boolean\n";
-    } else if (is_array($random)) {
-        echo "{$random} is an array\n";
-    } else if (is_null($random)) {
-        echo "{$random} is bull\n";
-    } else if (is_string($random)) {
-        echo "{$random} is a string\n";
+foreach ($entireArray as $objectsWithinArray) {
+    if (is_integer($objectsWithinArray)) {
+        echo "{$objectsWithinArray} is an array\n";
+    } else if (is_float($objectsWithinArray)) {
+        echo "{$objectsWithinArray} is a float\n";
+    } else if (is_bool($objectsWithinArray)) {
+        echo "false is a boolean\n";
+    } else if (is_array($objectsWithinArray)) {
+        foreach ($objectsWithinArray as $valuesWithinArray) {
+            echo "$valuesWithinArray\n";
+        }
+    } else if (is_null($objectsWithinArray)) {
+        echo "the value of null is null\n";
+    } else if (is_string($objectsWithinArray)) {
+        echo "{$objectsWithinArray} is a string\n";
     }
 }
+
+// foreach($entireArray as $objectsWithinArray){
+//     if (is_scalar($objectsWithinArray))
+//         echo "{$objectsWithinArray} is scalar\n";
+// }
+
+// foreach($entireArray as $objectsWithinArray){
+//     echo "{$objectsWithinArray}\n";
+// }
