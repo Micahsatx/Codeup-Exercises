baf68dfd9dc766c1ed05d134a924c81bb0282356
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-18 12:56:04 -0500

    cleaned up code and made minor changes

--- 99bottles.php
+++ 99bottles.php
@@ -2,8 +2,20 @@
 
 $bottlesOfBeerOnWall = 99;
 
-do {
+
     echo "{$bottlesOfBeerOnWall} bottles of beer on the wall. {$bottlesOfBeerOnWall} bottles of beer.";
     echo " Take one down pass it around" . ' ' . ($bottlesOfBeerOnWall - 1) . ' ' . "bottles of beer on the wall.\n";
     $bottlesOfBeerOnWall--;
+
+
+do{    
+    if ($bottlesOfBeerOnWall <= 1){
+        echo "{$bottlesOfBeerOnWall} bottle of beer on the wall. {$bottlesOfBeerOnWall} bottle of beer.";
+        echo " Take one down pass it around" . ' ' . ($bottlesOfBeerOnWall - 1) . ' ' . "bottles of beer on the wall.\n";
+        $bottlesOfBeerOnWall--;
+    } else {
+        echo "{$bottlesOfBeerOnWall} bottles of beer on the wall. {$bottlesOfBeerOnWall} bottles of beer.";
+    echo " Take one down pass it around" . ' ' . ($bottlesOfBeerOnWall - 1) . ' ' . "bottles of beer on the wall.\n";
+    $bottlesOfBeerOnWall--;
+    }
 } while ($bottlesOfBeerOnWall >= 1);
