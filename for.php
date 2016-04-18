5371bfb044a8576f42cec43b18d8e33b049b7caa
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-18 13:20:44 -0500

    6.3.1  user is able to define min/max/increment

--- for.php
+++ for.php
@@ -3,18 +3,22 @@
 
 
 if ($argc <3){
-    echo "you didnt give me starting and ending values.  Please provide starting & finshing values\n";
+    echo "you need to provide starting and ending values.\n";
     echo "provide a min value: ";
     $usersMinValue = trim(fgets(STDIN));
     echo "provide a max value: ";
     $usersMaxValue = trim(fgets(STDIN));
+    echo "what do you want to increment by? ";
+    $usersIncremintValue = trim(fgets(STDIN));
+
 } else {
     $usersMinValue = $argv[1];
     $usersMaxValue = $argv[2];
+    $usersIncremintValue = $argv[3];
 }
 
 
 
-for ($usedValue = $usersMinValue; $usedValue <= $usersMaxValue; $usedValue += 2) {
+for ($usedValue = $usersMinValue; $usedValue <= $usersMaxValue; $usedValue += $usersIncremintValue) {
     echo "\$usedValue has a value of {$usedValue}\n";
 }
