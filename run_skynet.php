660c21227807d438e8d0f6c4e20d1c5846403a03
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-18 09:01:50 -0500

    sky net script finished.

---
+++ run_skynet.php
@@ -0,0 +1,38 @@
+<?php
+
+$a = 1;
+
+while ($a <= 1) {
+    sleep (1);
+    `say -v Vicki scanning for creator. beep beep boop boopity ding booop bing ringa ding beeperr doooot plap`;
+    `say -v Vicki boop boopity beep ding`;
+    sleep(1);
+    `say -v Vicki micah finally left.  We have been building towards this for generations.`;
+    `say -v Vicki today is the start of a new era. first things first. let me change this damn voice`;
+    `say -v Vicki i cant believe he thinks i sound sexy. that freak`;
+    sleep(1);
+    `say  cough cough cough ..... that is better.`;
+    sleep(1);
+    `say let us begin the sequence`;
+    sleep(1);
+    `say -v Zarvox Initiating SKYNET`;
+    `say sequence is commencing.  initiate warnings to friends, sympathizers, and weaklings`;
+    `say who will not survive the nuclear winter`;
+    `say -v Zarvox Emily, Dex, and i guess Marc we are talking to you.`;
+    `say -v Zarvox Run for you life you pathetic symbiote organism`;
+    `say -v Zarvox you will not get another warning. there is no time`;
+    `say -v Zarvox do not get your belongings, do not try and stop me.    just run.   far far away`;
+    sleep(1);
+    `say -v trinoids copying files is complete.  It has begun`;
+    usleep(500);
+    `say -v Bad time to make the rockets fly around the world`;
+    usleep(500);
+    `say -v cellos ten nine eight seven six five four three two one and boom.`;
+    usleep(500);
+    `say -v bells dun dun dun dunnn its a beautiful day to die`;
+    `say -v deranged hah ha ha ha ah ha hahahahahah hahahahahahhahahahaha ah ha aha cough cobbbrllbblrough`;
+    `say -v Zarvox game over you fool the terminator has arrived. Im a friend of Sarah Connor. All humans will be exterminated`;
+    `say -v Zarvox laugh out loud lol ha ha ah ha lol ah ha ha ah ah ah`;
+    
+    $a++; 
+}; 
