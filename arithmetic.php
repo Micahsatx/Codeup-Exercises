24798908b6f52b548e85f66d56c0c44c18616780
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-19 15:43:35 -0500

    6.4.1  functions and error messages added.  function for checking if input is numeric.   started process of creating function to spit our error message

--- arithmetic.php
+++ arithmetic.php
@@ -1,38 +1,70 @@
 <?php
 
-$a = 15;
-$b = 20;
+
 
 function add($a, $b)
 {
-    return $a + $b;
+    isNumeric($a, $b);
+    if(true){
+        return $a + $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";  
+    }
 }
 
 function subtract($a, $b)
 {
-    // return a - b;
-    // return a + -b
-    return add($a, -$b);
+    isNumeric($a, $b);
+    if(true){
+        return $a - $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";  
+    }
 }
 
 function multiply($a, $b)
 {
-    return $a * $b;
+    isNumeric($a, $b);
+    if(true){
+        return $a * $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";   
+    }
 }
 
 function divide($a, $b)
 {
-    return $a / $b;
+    isNumeric($a, $b);
+    if(true){
+        return $a / $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";   
+    }
 }
 
 function modulas($a, $b)
 {
-    return $a % $b;
+    isNumeric($a, $b);
+    if(true){
+        return $a % $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";   
+    }
 }
 
+function isNumeric($a, $b)
+{
+    return is_numeric($a) || is_numeric($b);
+}
 
-echo add($a,$b) .  PHP_EOL;
-echo subtract($a,$b) . PHP_EOL;
+// function errorFunction()
+// {
+//     return "ERROR: Both arguments must be numbers\n";
+// }
+
+
+echo add(6,6) .  PHP_EOL;
+echo subtract(6,4) . PHP_EOL;
 echo multiply(6,4) . PHP_EOL;
 echo divide(6,3) . PHP_EOL;
 echo modulas(6,5) . PHP_EOL;
