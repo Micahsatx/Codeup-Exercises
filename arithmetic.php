4fb1f96adc4ffad2f24b0aa95395ef541e6ccb7c
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-19 14:40:38 -0500

    6.4  messing with functions calling functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,8 @@
 <?php
 
+$a = 15;
+$b = 20;
+
 function add($a, $b)
 {
     return $a + $b;
@@ -7,7 +10,9 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    return $a - $b;
+    // return a - b;
+    // return a + -b
+    return add($a, -$b);
 }
 
 function multiply($a, $b)
@@ -20,8 +25,14 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulas($a, $b)
+{
+    return $a % $b;
+}
 
-echo add(6,4) .  PHP_EOL;
-echo subtract(6,4) . PHP_EOL;
+
+echo add($a,$b) .  PHP_EOL;
+echo subtract($a,$b) . PHP_EOL;
 echo multiply(6,4) . PHP_EOL;
 echo divide(6,3) . PHP_EOL;
+echo modulas(6,5) . PHP_EOL;
