303e8e5767cb9e47c17c2d3126f777494f4f92e2
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-22 09:53:38 -0500

    6.6.1 contact parser exercise complete.  notes added, code and variables cleaned up going to attempt to break down into a couple functions

--- contact-parser.php
+++ contact-parser.php
@@ -1,52 +1,56 @@
 <?php
-// name of file
-// $filename = 'cities.txt';
-
-// // connection(bookmark)
-// // 'r' means you can read it.. not write, not append, ONLY read
-// $handle = fopen($filename, 'r');
-
-// // gets the contents of the file from the $handle(bookmark)
-// $contents = fread($handle, filesize($filename));
-// // dont forget to do this EVERYTIME
-// fclose($handle);
-
-// $array = explode('\n', $contents)
-
-
 
 $filename = 'contacts.txt';
 
 function parseContacts($filename)
 {
+    // establish a connection to the file ($handle)
     $handle = fopen($filename, 'r');
+    // set the file its reading equal to $contents
+    // filesize is the actualy size of the file
+    // and $filename is the variable assigned to the actual file
     $contents = fread($handle, filesize($filename));
     
-    $contacts = explode("\n", $contents);
-    array_pop($contacts);
+    // the file starts as a string and explodes it into an array of 4
+    // then sets it equal to $contacts 
+    $eachContact = explode("\n", $contents);
+    // theres an extra array since it explodes at the end of a new line
+    array_pop($eachContact);
     
-    $newContacts = [];
+    // the array that the info within the function will be pushed to.
+    $finalArrayOfContacts = [];
     fclose($handle);
 
-    foreach ($contacts as $person) {
-        $newArray = [];
-        $contactsInfo = explode("|", $person);
-        $name = $contactsInfo[0];
-        $number = substr ($contactsInfo[1], 0, 3) . "-" . substr ($contactsInfo[1], 3, 3) . "-" . substr ($contactsInfo[1], 6, 4);
-        $newArray = ['name' => $name, 'number' => $number];
-        array_push($newContacts, $newArray);
-
-    // string substr ( string $string , int $start [, int $length ] )
-    // $string = 'bcadef abcdef';
-    // $substr = 'a';
-    // $attachment = '+++';
-
-    // //$position = strpos($string, 'a');
-
-    // $newstring = str_replace($substr, $substr.$attachment, $string);
+    foreach ($eachContact as $individualPersonNameAndNumber) {
+        // new array which will be used to push the inner 2 arrays onto
+        $associativeArrayWithNameNumber = [];
+        // the name of the each array that contains both name and number
+        // but that array is not yet split into 2 arrays
+        $individualsContactInfo = explode("|", $individualPersonNameAndNumber);
+        // assigning the index 0 & 1 to a variable
+        
+        // inputting the '-' into a specific location in the phone number
+        // putting the first one in at the start of the string[0] and a length of 3
+        $areaCode = substr ($individualsContactInfo[1], 0, 3);
+        // putting the second '-' starting at index 3 and going 3 spaces from there
+        $firstThree = substr ($individualsContactInfo[1], 3, 3);
+        // start at index 6 and go 4 more
+        $lastFour = substr ($individualsContactInfo[1], 6, 4);
+        // now concatinate the 3 variables with '-' in the middle
+        // assign that into $number
+        $number =  "{$areaCode}-{$firstThree}-{$lastFour}";
+        // name variable and number variable accessed using there index of
+        // $individualsContactInfo
+        $name = $individualsContactInfo[0];
+        // splitting $individualsContactInfo into 2 arrays within it.  one array that
+        // puts the names into names and the numbers into numbers
+        // this array split into 2 is now called associativeArrayWithNameNumber
+        $associativeArrayWithNameNumber = ['name' => $name, 'number' => $number];
+        // now push that created split array onto the $finalArrayOfContacts
+        array_push($finalArrayOfContacts, $associativeArrayWithNameNumber);
 
     }
-    return $newContacts;
+    return $finalArrayOfContacts;
 }
 var_dump(parseContacts('contacts.txt'));  
 
