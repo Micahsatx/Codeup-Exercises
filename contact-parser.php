de8bd8f8cb3803d9cd0cc3793b6e6707ad17b7e2
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-22 10:28:31 -0500

    6.6.1 contact parser.  added a second function and changed notes.  a few variable names. planning on further splitting function

--- contact-parser.php
+++ contact-parser.php
@@ -2,6 +2,33 @@
 
 $filename = 'contacts.txt';
 
+// this function takes in exploded array then puts them to variables
+// name and number then puts that into an array containing
+// one name and one number
+function dashesAndArrayOfIndividuals($explodedArrayWithNameAndNumber)
+{
+    // inputting the '-' into a specific location in the phone number
+    // putting the first one in at the start of the string[0] and a length of 3
+    $areaCode = substr ($explodedArrayWithNameAndNumber[1], 0, 3);
+    // putting the second '-' starting at index 3 and going 3 spaces from there
+    $firstThree = substr ($explodedArrayWithNameAndNumber[1], 3, 3);
+    // start at index 6 and go 4 more
+    $lastFour = substr ($explodedArrayWithNameAndNumber[1], 6, 4);
+    // now concatinate the 3 variables with '-' in the middle
+    // assign that into $number
+    $number =  "{$areaCode}-{$firstThree}-{$lastFour}";
+    // name variable and number variable accessed using there index of
+    // $individualsContactInfo
+    $name = $explodedArrayWithNameAndNumber[0];
+    // splitting $individualsContactInfo into 2 arrays within it.  one array that
+    // puts the names into names and the numbers into numbers
+    // this array split into 2 is now called associativeArrayWithNameNumber
+    $associativeArrayWithNameNumber = ['name' => $name, 'number' => $number];
+    // now push that created split array onto the $finalArrayOfContacts
+    return $associativeArrayWithNameNumber;
+} 
+
+
 function parseContacts($filename)
 {
     // establish a connection to the file ($handle)
@@ -10,45 +37,28 @@ function parseContacts($filename)
     // filesize is the actualy size of the file
     // and $filename is the variable assigned to the actual file
     $contents = fread($handle, filesize($filename));
-    
+    // needs to happen before the explode so its trimming a string
+    $eachContact = trim($eachContact);
     // the file starts as a string and explodes it into an array of 4
     // then sets it equal to $contacts 
     $eachContact = explode("\n", $contents);
-    // theres an extra array since it explodes at the end of a new line
-    array_pop($eachContact);
     
-    // the array that the info within the function will be pushed to.
-    $finalArrayOfContacts = [];
     fclose($handle);
+    // emtpy array that the array with name and number ($associativeArrayWithNameNumber)
+    // gets pushed into 
+    $finalArrayOfContacts = [];
 
     foreach ($eachContact as $individualPersonNameAndNumber) {
-        // new array which will be used to push the inner 2 arrays onto
-        $associativeArrayWithNameNumber = [];
-        // the name of the each array that contains both name and number
-        // but that array is not yet split into 2 arrays
+        // breaking the string of names and numbers and splitting them into
         $individualsContactInfo = explode("|", $individualPersonNameAndNumber);
-        // assigning the index 0 & 1 to a variable
+        // call the function and pass it individualsContactInfo
+        // the exploded sring seperated into name and number
+        // set all that equal to eachIndividualArray variable
+        $eachIndividualArray = dashesAndArrayOfIndividuals($individualsContactInfo);
+        // so each iteration of the loop pushes eachIndividualArray
+        // onto the final Array
+        array_push($finalArrayOfContacts, $eachIndividualArray);
         
-        // inputting the '-' into a specific location in the phone number
-        // putting the first one in at the start of the string[0] and a length of 3
-        $areaCode = substr ($individualsContactInfo[1], 0, 3);
-        // putting the second '-' starting at index 3 and going 3 spaces from there
-        $firstThree = substr ($individualsContactInfo[1], 3, 3);
-        // start at index 6 and go 4 more
-        $lastFour = substr ($individualsContactInfo[1], 6, 4);
-        // now concatinate the 3 variables with '-' in the middle
-        // assign that into $number
-        $number =  "{$areaCode}-{$firstThree}-{$lastFour}";
-        // name variable and number variable accessed using there index of
-        // $individualsContactInfo
-        $name = $individualsContactInfo[0];
-        // splitting $individualsContactInfo into 2 arrays within it.  one array that
-        // puts the names into names and the numbers into numbers
-        // this array split into 2 is now called associativeArrayWithNameNumber
-        $associativeArrayWithNameNumber = ['name' => $name, 'number' => $number];
-        // now push that created split array onto the $finalArrayOfContacts
-        array_push($finalArrayOfContacts, $associativeArrayWithNameNumber);
-
     }
     return $finalArrayOfContacts;
 }
