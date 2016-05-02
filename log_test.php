7b8e4090a6a33284f0d78205080092ffdc71d34a
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-02 11:06:56 -0500

    exerrcise 7.3  starting objects, & methods

---
+++ log_test.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'Log.php';
+date_default_timezone_set('America/Chicago');
+
+$fileName = new Log();
+
+$fileName-> logMessage("INFO","This is the log level message");
+$fileName-> logInfo("This is an INFO message");
+$fileName-> logError("This is an ERROR message");
