b9ce982ee99d246269931cec073e403b7cafb2b1
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-02 11:08:16 -0500

    exercise 7.3  methods, objects

---
+++ Log.php
@@ -0,0 +1,39 @@
+<?php
+
+class Log
+{
+    public $fileName;
+
+    public function logMessage($logLevel, $message)
+    {
+        // get the date
+        $currentDate = date("Y-m-d");
+        // get the time
+        $currentTime = date('h:i:s');
+        // make a file name and use a string to say log.2016-05-19.log
+        $filename = "log-$currentDate.log";
+        // making the connection to the filename specified above
+        $handle = fopen($filename, 'a');
+
+        // use handle for connection
+        // $currentDate . $currentTime . then "[level]" (this comes from $loglevel)
+        // $logLevel comes from the first value in the logmessage function, which is INFO or ERROR
+        fwrite($handle, $currentDate . ' ' . $currentTime . "[$logLevel]" . $message . PHP_EOL);
+        fclose($handle);
+    }
+
+    // message will be provided when the function is called!
+    function logInfo($message)
+    {
+        $this ->logMessage("INFO", $message);
+    }
+
+    // message will be provided when the function is called!
+    function logError($message)
+    {
+        $this ->logMessage("ERROR", $message);
+    }
+}
+
+
+?>
