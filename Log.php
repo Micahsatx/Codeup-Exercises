557718e9b9bb8953557ee89d6ef489546dd266f4
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-02 14:18:55 -0500

    7.3.1 construct and destruct methods.

--- Log.php
+++ Log.php
@@ -3,35 +3,53 @@
 class Log
 {
     public $fileName;
-
+    public $handle;
+    public $currentDate;
+    public $currentTime;
+    
     public function logMessage($logLevel, $message)
     {
         // get the date
-        $currentDate = date("Y-m-d");
+        $this->currentDate = date("Y-m-d");
         // get the time
-        $currentTime = date('h:i:s');
-        // make a file name and use a string to say log.2016-05-19.log
-        $filename = "log-$currentDate.log";
-        // making the connection to the filename specified above
-        $handle = fopen($filename, 'a');
-
+        $this->currentTime = date('h:i:s');
         // use handle for connection
         // $currentDate . $currentTime . then "[level]" (this comes from $loglevel)
         // $logLevel comes from the first value in the logmessage function, which is INFO or ERROR
-        fwrite($handle, $currentDate . ' ' . $currentTime . "[$logLevel]" . $message . PHP_EOL);
-        fclose($handle);
+        fwrite($this->handle, $this->currentDate . ' ' . $this->currentTime . "[$logLevel]" . $message . PHP_EOL);
+        
+    }
+            
+        
+    public function __construct($prefix)
+    {
+        // $prefix = log;
+        // make a file name and use a string to say log.2016-05-19.log
+        // $this->filename = "$prefix-$currentDate.log";
+        $this->fileName = "$prefix-YYYY-MM-DD.log";
+
+        // making the connection to the filename specified above
+        $this->handle = fopen($this->fileName, 'a');
+
+    }
+
+    public function __destruct()
+    {
+
+        fclose($this->handle);
+        echo "did it work?\n";
     }
 
     // message will be provided when the function is called!
-    function logInfo($message)
+    public function logInfo($message)
     {
-        $this ->logMessage("INFO", $message);
+        $this->logMessage("INFO", $message);
     }
 
     // message will be provided when the function is called!
-    function logError($message)
+    public function logError($message)
     {
-        $this ->logMessage("ERROR", $message);
+        $this->logMessage("ERROR", $message);
     }
 }
 
--- log_test.php
+++ log_test.php
@@ -2,8 +2,8 @@
 require_once 'Log.php';
 date_default_timezone_set('America/Chicago');
 
-$fileName = new Log();
+$fileName = new Log('cli');
 
-$fileName-> logMessage("INFO","This is the log level message");
-$fileName-> logInfo("This is an INFO message");
-$fileName-> logError("This is an ERROR message");
+$fileName->logMessage("INFO","This is the log level message");
+$fileName->logInfo("This is an INFO message");
+$fileName->logError("This is an ERROR message");
